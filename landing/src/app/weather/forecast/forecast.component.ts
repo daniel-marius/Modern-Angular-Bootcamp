14fa980cfa846888e183b7b2f680c1984241c8f1
danielmarius1995 <danieladam01995@gmail.com>
2022-01-03 22:53:44 +0200

    Latest commit

---
+++ landing/src/app/weather/forecast/forecast.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ForecastService } from '../forecast.service';
+
+@Component({
+  selector: 'app-forecast',
+  templateUrl: './forecast.component.html',
+  styleUrls: ['./forecast.component.css']
+})
+export class ForecastComponent implements OnInit {
+  forecast$: Observable<{ dateString: string; temp: number; }[]>;
+
+  constructor(forecastService: ForecastService) {
+    this.forecast$ = forecastService.getForecast();
+  }
+
+  ngOnInit() {}
+}
