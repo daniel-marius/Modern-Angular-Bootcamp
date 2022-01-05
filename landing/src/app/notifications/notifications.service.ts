9ecc3832f1a28f4f28f2a453554c23e08cdf5013
danielmarius1995 <danieladam01995@gmail.com>
2022-01-05 23:10:30 +0200

    New commit

--- landing/src/app/notifications/notifications.service.ts
+++ landing/src/app/notifications/notifications.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { scan } from 'rxjs/operators';
+
+interface Command {
+  id: number;
+  type: 'success' | 'error' | 'clear';
+  text?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
+  messages: Subject<any>;
 
-  constructor() { }
+  constructor() {
+    this.messages = new Subject<Command>();
+  }
+
+  getMessages() {
+    return this.messages.pipe(
+      scan((acc: Command[], value: Command) => {
+        if (value.type === 'clear') {
+          return acc.filter(message => message.id !== value.id);
+        } else {
+          return [...acc, value];
+        }
+      }, [])
+    );
+  }
+
+  addSuccess(message: string) {
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'success'
+    });
+  }
+
+  addError(message: string) {
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'error'
+    });
+  }
+
+  clearMessage(id: number) {
+    this.messages.next({
+      id,
+      type: 'clear'
+    });
+  }
+
+  private randomId() {
+    return Math.round(Math.random() * 10000);
+  }
 }
